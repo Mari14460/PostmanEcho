ff2c274999faa712177c0175113befdc72ce0b0a
Marina Zinchenko <amanda79116@gmail.com>
2022-12-11 22:30:16 -0600

    Removed comments

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -7,14 +7,11 @@ public class PostmanEchoTest {
     @Test
     void shouldReturnDemoAccounts() {
         // Given - When - Then
-        // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("Marina") // отправляемые данные (заголовки и query можно выставлять аналогично)
-                // Выполняемые действия
+                .body("Marina")
                 .when()
                 .post("/post")
-                // Проверки
                 .then()
                 .statusCode(200)
                 .body("data", equalTo("Marina"));
